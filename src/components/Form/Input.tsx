2926a5fe119a5a9a529a514b675c71da7da43d37
Luis Fernando <luis.silva9902@gmail.com>
2022-08-30 14:56:51 -0300

    Creating form validation with the yup library

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,16 +1,24 @@
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { FormControl, FormErrorMessage, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
 import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps{
   name: string;
-  label?: string
+  label?: string;
+  error?: FieldError
 }
 
-const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) => {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, error = null,  ...rest }, ref) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
       <ChakraInput name={name} id={name} type={'email'} focusBorderColor={'pink.500'} bgColor={'gray.900'} variant={'filled'} _hover={{bgColor: 'gray.900'}} size={'lg'} {...rest} ref={ref}/>
+
+      { !!error && (
+        <FormErrorMessage>
+          {error.message}
+        </FormErrorMessage>
+      ) }
     </FormControl>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,27 @@ import type { NextPage } from 'next'
 import { Button, Flex, Stack } from '@chakra-ui/react'
 import { Input } from '../components/Form/Input'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 type SignInFormData = {
   email: string;
-  password: string
+  password: string;
 }
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha orbigatória'),
+})
 
 const SignIn: NextPage = () => {
 
-  const { register, handleSubmit, formState } = useForm<SignInFormData>()
+  const { register, handleSubmit, formState } = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema)
+  })
+  const { errors } = formState
 
-  const handleSignIn: SubmitHandler<SignInFormData>  = async (values, event) => {
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values, event) => {
     await new Promise(resolve => setTimeout(resolve, 2000))
     console.log(values)
   }
@@ -22,8 +32,8 @@ const SignIn: NextPage = () => {
       <Flex as={'form'} width="100%" maxWidth={360} bg={'gray.800'} p="8" borderRadius={8} flexDir={'column'} onSubmit={handleSubmit(handleSignIn)}>
 
         <Stack spacing={4}>
-          <Input type={'email'} label="E-mail" {...register('email')}/>
-          <Input type={'password'} label="Senha" {...register('password')}/>
+          <Input type={'email'} label="E-mail" error={errors.email} {...register('email')} />
+          <Input type={'password'} label="Senha" error={errors.password} {...register('password')} />
         </Stack>
 
         <Button type='submit' mt={'6'} colorScheme={'pink'} size={'lg'} isLoading={formState.isSubmitting}>Entrar</Button>
