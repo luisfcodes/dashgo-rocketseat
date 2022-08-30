db561b00531907c062fb4550df39a9241c6c8e33
Luis Fernando <luis.silva9902@gmail.com>
2022-08-30 12:01:57 -0300

    Addition of form-hook

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,15 +1,18 @@
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps{
   name: string;
   label?: string
 }
 
-export function Input({ name, label, ...rest }: InputProps){
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) => {
   return (
     <FormControl>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
-      <ChakraInput name={name} id={name} type={'email'} focusBorderColor={'pink.500'} bgColor={'gray.900'} variant={'filled'} _hover={{bgColor: 'gray.900'}} size={'lg'} {...rest}/>
+      <ChakraInput name={name} id={name} type={'email'} focusBorderColor={'pink.500'} bgColor={'gray.900'} variant={'filled'} _hover={{bgColor: 'gray.900'}} size={'lg'} {...rest} ref={ref}/>
     </FormControl>
   )
 }
+
+export const Input = forwardRef(InputBase)
